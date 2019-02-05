bfc85b994a393a1ce870b0f4d43d9446f354897f
Valentin Dobrica <valentin.dobrica@rei-d-services.com>
2019-02-05 15:05:54 +0200

    REACT GOD

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './Person.css';
-import Radium from 'radium';
+import React, {Component} from 'react';
+import classes from './Person.css';
 
-const person = ( props ) => {
+const person = (props) => {
+
+    const rnd = Math.random();
+    console.log("Random number = " + rnd);
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'am {props.name} and i am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
